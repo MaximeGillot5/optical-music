b4cea42f6d278292cd4039a05cb8c23354cf0a0e
MaximeGillot5 <mgillot56@icloud.com>
2024-09-26 01:13:38 +0200

    sound add and basic bar for changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import { useRef, useState } from "react";
 import Circle from "../components/Circle";
+import Navbar from "../components/Navbar";
 
 const Home = () => {
 
   return (
     <main className="bg-black h-screen grid place-items-center text-white">
+        <Navbar />
         <Circle />
     </main>
   );
